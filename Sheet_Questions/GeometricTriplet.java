23ec3d515ce7deecc7d13ee536d423c8e344e8a9
Arpita-1406 <pandeyarpita013@gmail.com>
2026-05-30 10:10:12 +0530

    update

---
+++ Sheet_Questions/GeometricTriplet.java
@@ -0,0 +1,29 @@
+package Sheet_Questions;
+
+public class GeometricTriplet {
+
+    public static void findTriplets(int arr[]) {
+        int n = arr.length;
+
+        System.out.println("Geometric Triplets:");
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+
+                    if (arr[j] * arr[j] == arr[i] * arr[k]) {
+                        System.out.println(arr[i] + " "
+                                + arr[j] + " "
+                                + arr[k]);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 4, 8, 16};
+
+        findTriplets(arr);
+    }
+}
